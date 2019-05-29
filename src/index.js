c384113d298e6e5da656840df6df9d28826d1044
Jose <sanchezmartinezjosecarlos@gmail.com>
2019-05-29 16:01:25 +0100

    Basic functionality- every tweet gets a new p tag & textarea cleared after click (#3) (#4)
    * Basic functionality - every new tweet gets a new p tag & counts characters & clears textarea

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', submitHandler);
-form.addEventListener('click', clearForm)
-
-function submitHandler(e) {
+function submitTweet() {
   event.preventDefault();
+  newTweet();
+  clearTextArea();
 }
 
 function newTweet() {
-  event.preventDefault();
   const tweet = document.getElementById('userInput').value;
   const newTweet = document.createElement('p');
   newTweet.setAttribute("id", "tweet-history")
   const parentNode = document.getElementById('tweets-container')
   parentNode.appendChild(newTweet)
-  newTweet.textContent = tweet
+  newTweet.textContent = tweet;
 }
 
-function clearForm() {
+function clearTextArea() {
   document.getElementById('userInput').value = ""
+  document.getElementById('counter').value = ""
+  counter.innerHTML = '0'
 }
+
+function charCounter() {
+  const len = event.target.value
+  len.length <= 20 ?
+    document.getElementById('counter').style.color = "green" :
+    document.getElementById('counter').style.color = "red";
+  counter.innerHTML = `${len.length}`
+}
+
+
